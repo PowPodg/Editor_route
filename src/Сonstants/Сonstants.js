e09d36f4048f6298b002dd063059249d924d6c90
PodgPow <oleg@oleg-iv.tk>
2019-11-11 12:26:12 +0300

    edit

--- src/Сonstants/Сonstants.js
+++ src/Сonstants/Сonstants.js
@@ -1,5 +1,5 @@
 module.exports = {
     PLACE_LOCAL: 'London',
     DELAY_PLACE_VISUAL_ERROR: 1000,
-    ICON_IMAGE_HREF: 'http://webmap-blog.ru/files/gmap/gicon/mm_20_red.png'
+    ICON_IMAGE_HREF: 'point.png'
 };
